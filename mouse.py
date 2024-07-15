3afcf08c21d43b57c8e7ef423d074c011f58c275
Ron Kiehn <ronthekiehn@gmail.com>
2024-07-14 22:43:53 -0500

    code

---
+++ mouse.py
@@ -0,0 +1,11 @@
+from pynput import mouse
+
+def on_move(x, y):
+    f = open("mouse.txt", "w")
+    x, y= int(x), int(y)
+    f.write(f"{x},{y}")
+    f.close()
+    #return False  # Stop listener
+
+with mouse.Listener(on_move=on_move) as listener:
+    listener.join()
